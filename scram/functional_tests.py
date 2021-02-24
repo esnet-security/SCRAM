24bdb4859d62dc1c7f1965eda3c7d40f83378198
Sam Oehlert <soehlert@es.net>
2021-02-24 10:27:37 -0600

    test(selenium): remove need for selenium; that's more geared towards js heavy webapps

--- scram/functional_tests.py
+++ scram/functional_tests.py
@@ -1,24 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 import unittest
 
 
 class HomePageTest(unittest.TestCase):
 
-	def setUp(self):
-		options = Options()
-		options.headless = True
-		self.browser = webdriver.Firefox(options=options)
-
-	def tearDown(self):
-		self.browser.quit()
-
-	def test_viewing_the_home_page(self):
-		# Go to the homepage
-		self.browser.get('http://localhost:8000')
-
-		# Make sure the title is correct
-		self.assertIn('SCRAM', self.browser.title)
+	pass
 
 
 if __name__ == "__main__":
