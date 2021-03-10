4cbd84ba6687bed6b36aa859fa9f7dc04a6e693a
Sam Oehlert <sam.oehlert@gmail.com>
2021-03-10 15:06:34 -0600

    feat(models): add beginning of ip model

--- scram/route_manager/models.py
+++ scram/route_manager/models.py
@@ -1,3 +1,7 @@
-# from django.db import models
+from django.db import models
+from netfields import InetAddressField
 
-# Create your models here.
+
+class IPAddress(models.Model):
+    """ Our base IP model """
+    ip = InetAddressField()
