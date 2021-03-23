2811546e3ebbc63e88a62291a73433f51177b6d0
Sam Oehlert <sam.oehlert@gmail.com>
2021-03-23 15:11:37 -0500

    refactor(view-names): be less verbose and more human readable for view names

--- scram/route_manager/urls.py
+++ scram/route_manager/urls.py
@@ -5,8 +5,8 @@ from .api import views
 app_name = 'route_manager'
 
 urlpatterns = [
-    path(route='api/', view=views.IPAddressListCreateAPIView.as_view(), name='ipaddress_rest_api'),
+    path(route='api/', view=views.NetworkView.as_view(), name='ipaddress_rest_api'),
     path(route='api/<path:ip>/',
-         view=views.IPAddressRetrieveUpdateDestroyAPIView.as_view(),
+         view=views.NetworkDetailView.as_view(),
          name='ipaddress_detail_rest_api')
 ]
