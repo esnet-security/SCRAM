e89d809172ecb8cf57215d74066aa7f7be5ca4df
Sam Oehlert <sam.oehlert@gmail.com>
2021-04-08 17:27:02 -0500

    fix(migration): ip should not be changed in the initial migration

--- scram/route_manager/migrations/0001_initial.py
+++ scram/route_manager/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='IPAddress',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('route', netfields.fields.InetAddressField(max_length=39)),
+                ('ip', netfields.fields.InetAddressField(max_length=39)),
             ],
         ),
     ]
