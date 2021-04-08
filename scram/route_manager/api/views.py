e38c32a1aa37e4d35299f323250bcac0f44707a9
Sam Oehlert <sam.oehlert@gmail.com>
2021-04-08 17:33:43 -0500

    refactor(api_views): change to route naming scheme

--- scram/route_manager/api/views.py
+++ scram/route_manager/api/views.py
@@ -2,18 +2,18 @@ from rest_framework.generics import (ListCreateAPIView, RetrieveDestroyAPIView)
 from rest_framework.permissions import IsAuthenticated
 
 from ..models import Route
-from .serializers import IPAddressSerializer
+from .serializers import RouteSerializer
 
 
-class NetworkView(ListCreateAPIView):
+class RouteView(ListCreateAPIView):
     queryset = Route.objects.all()
     permission_classes = (IsAuthenticated,)
-    serializer_class = IPAddressSerializer
+    serializer_class = RouteSerializer
     lookup_field = 'uuid'
 
 
-class NetworkDetailView(RetrieveDestroyAPIView):
+class RouteDetailView(RetrieveDestroyAPIView):
     queryset = Route.objects.all()
     permission_classes = (IsAuthenticated,)
-    serializer_class = IPAddressSerializer
+    serializer_class = RouteSerializer
     lookup_field = 'route'
