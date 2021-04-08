bf960f022d1c5e76e5b6ceaaadf411a533478d76
Sam Oehlert <sam.oehlert@gmail.com>
2021-04-08 17:31:34 -0500

    refactor(serializer): change name to reflect "route" naming scheme

--- scram/route_manager/api/serializers.py
+++ scram/route_manager/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from ..models import Route
 
 
-class IPAddressSerializer(serializers.ModelSerializer):
+class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
         fields = ['route', 'uuid']
