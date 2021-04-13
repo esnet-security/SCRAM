7441d58c56f7879dfe6de5e7649130955f472348
Sam Oehlert <sam.oehlert@gmail.com>
2021-04-13 13:07:40 -0500

    feat(admin): add entry to admin site

--- scram/route_manager/admin.py
+++ scram/route_manager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Route, ActionType
+from .models import ActionType, Entry, Route
 
 
 admin.site.register(Route)
 admin.site.register(ActionType)
+admin.site.register(Entry)
