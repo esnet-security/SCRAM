67245b6e97bdd93d7dafd3873577cacaadf563cb
Sam Oehlert <sam.oehlert@gmail.com>
2021-04-13 17:25:16 -0500

    feat(drf-router): use the drf router for api/ instead of manually creating our own

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from scram.users.api.views import UserViewSet
+from scram.route_manager.api.views import ActionTypeViewSet, RouteViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,6 +10,8 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("actiontypes", ActionTypeViewSet)
+router.register("routes", RouteViewSet)
 
 
 app_name = "api"
--- scram/route_manager/urls.py
+++ scram/route_manager/urls.py
@@ -5,8 +5,4 @@ from .api import views
 app_name = 'route_manager'
 
 urlpatterns = [
-    path(route='api/', view=views.RouteView.as_view(), name='api_root'),
-    path(route='api/<path:route>/',
-         view=views.RouteDetailView.as_view(),
-         name='api_route_detail')
 ]
