d19821936195f54eba56132f1d9dab4762fc7741
Sam Oehlert <sam.oehlert@gmail.com>
2021-04-14 13:27:05 -0500

    test(initial-data): check to see if our initial actiontype of block exists out of the box

---
+++ scram/route_manager/tests/acceptance/features/initial_data.feature
@@ -0,0 +1,8 @@
+Feature: Out of the box initial data
+
+    Scenario: As a BHR replacement, it comes with a block actiontype
+    When we're logged in
+    And  we list the actiontypes
+
+    Then we get a 200 status code
+    And the number of actiontypes is 1
--- scram/route_manager/tests/acceptance/steps/common.py
+++ scram/route_manager/tests/acceptance/steps/common.py
@@ -1,5 +1,7 @@
 from behave import then, when
 
+from django.urls import reverse
+
 
 @when("we're logged in")
 def step_impl(context):
@@ -9,3 +11,16 @@ def step_impl(context):
 @then("we get a {status_code:d} status code")
 def step_impl(context, status_code):
     context.test.assertEqual(context.response.status_code, status_code)
+
+
+@when("we list the actiontypes")
+def step_impl(context):
+    context.response = context.test.client.get(
+        reverse("api:actiontype-list")
+    )
+
+
+@then("the number of actiontypes is {num:d}")
+def step_impl(context, num):
+    objs = context.test.client.get(reverse("api:actiontype-list"))
+    context.test.assertEqual(len(objs.json()), num)
