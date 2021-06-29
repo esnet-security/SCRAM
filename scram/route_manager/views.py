1413bf22e3e9e808d54669cb9a42cb3ca17e5855
Sam Oehlert <soehlert@es.net>
2021-06-29 17:39:45 -0500

    feat(default-user): if no user exists; create an admin account and let the user know it happened as well as the random password django created

--- scram/route_manager/views.py
+++ scram/route_manager/views.py
@@ -1,10 +1,22 @@
+from django.contrib import messages
 from django.shortcuts import render
 
+from ..users.models import User
 from .models import ActionType, Entry
 
 
 def home_page(request):
     context = {"Entries": Entry.objects.all(), "Actiontypes": ActionType.objects.all()}
+
+    if User.objects.count() == 0:
+        password = User.objects.make_random_password()
+        User.objects.create_superuser("admin", "admin@example.com", password)
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            f"Admin user created for you.\n Please save this password: {password}",
+        )
+
     return render(request, "route_manager/home.html", context)
 
 
