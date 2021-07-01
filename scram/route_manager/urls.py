42c70d26dc47d4c1b7fba1648611fecf80b40da8
Sam Oehlert <soehlert@es.net>
2021-07-01 15:52:44 -0500

    style(precommit): ran black, isort, flake8

--- scram/route_manager/urls.py
+++ scram/route_manager/urls.py
@@ -7,5 +7,6 @@ app_name = "route_manager"
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("search/", views.search_entries, name="search"),
+    path("delete/<int:pk>/", views.delete_entry, name="delete"),
     path(route="<int:pk>/", view=views.EntryDetailView.as_view(), name="detail"),
 ]
