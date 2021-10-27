d26a3a91f0f776684ae3db9352ba44812534de16
Sam Oehlert <soehlert@es.net>
2021-10-27 12:48:29 -0500

    fix(context): do the reverse in context processor so we just pass the raw path to our template
    it removes some logic from templates which is good

--- scram/route_manager/context_processors.py
+++ scram/route_manager/context_processors.py
@@ -1,5 +1,8 @@
 from django.conf import settings
+from django.urls import reverse
 
 
 def login_logout(request):
-    return {"login": settings.LOGIN_URL, "logout": settings.LOGOUT_URL}
+    login_url = reverse(settings.LOGIN_URL)
+    logout_url = reverse(settings.LOGOUT_URL)
+    return {"login": login_url, "logout": logout_url}
