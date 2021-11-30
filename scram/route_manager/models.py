04597682a5099fccb95f08696a4bad68e67d48bf
Sam Oehlert <soehlert@es.net>
2021-11-30 17:08:18 -0600

    style(precommit): run hooks

--- scram/route_manager/models.py
+++ scram/route_manager/models.py
@@ -6,7 +6,7 @@ from netfields import CidrAddressField
 
 
 class Route(models.Model):
-    """ Model describing a route """
+    """Model describing a route"""
 
     route = CidrAddressField(unique=True)
     uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
@@ -19,7 +19,7 @@ class Route(models.Model):
 
 
 class ActionType(models.Model):
-    """ Defines an action that can be done with a given route. e.g. Block, shunt, redirect, etc. """
+    """Defines an action that can be done with a given route. e.g. Block, shunt, redirect, etc."""
 
     name = models.CharField(
         help_text="One-word description of the action", max_length=30
@@ -35,23 +35,30 @@ class ActionType(models.Model):
 
 
 class Entry(models.Model):
-    """ An instance of an action taken on a route. """
+    """An instance of an action taken on a route."""
 
     route = models.ForeignKey("Route", on_delete=models.PROTECT)
     actiontype = models.ForeignKey("ActionType", on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         unique_together = ["route", "actiontype"]
 
+    def __str__(self):
+        desc = f"{self.route} ({self.actiontype})"
+        if not self.is_active:
+            desc += " (inactive)"
+        return desc
+
 
 class History(models.Model):
-    """ Who, what, when, why """
+    """Who, what, when, why"""
 
     entry = models.ForeignKey("Entry", on_delete=models.CASCADE)
-    who = models.CharField("Username", default="Unknown", max_length=30)
+
     why = models.CharField("Comment for the action", max_length=200)
     when = models.DateTimeField(auto_now_add=True)
-
+    who = models.CharField("Username", default="Unknown", max_length=30)
     expiration = models.DateTimeField(default="9999, 12, 31, tz=timezone.utc")
     expiration_reason = models.CharField(
         help_text="Optional reason for the expiration",
