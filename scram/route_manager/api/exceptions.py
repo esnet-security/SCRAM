e8def9a7841ebec560093d81831b9b9c97237bb5
Sam Oehlert <sam.oehlert@gmail.com>
2022-02-28 15:20:16 -0600

    feat(prefixtoolarge): automatically populate the detailed message based on our settings file

--- scram/route_manager/api/exceptions.py
+++ scram/route_manager/api/exceptions.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from rest_framework.exceptions import APIException
 
 
 class PrefixTooLarge(APIException):
+    v4_min_prefix = getattr(settings, f"V4_MINPREFIX", 0)
+    v6_min_prefix = getattr(settings, f"V6_MINPREFIX", 0)
     status_code = 400
-    default_detail = "You've supplied too large of a network. Defaults: v4 >= 8 v6 >=32"
+    default_detail = f"You've supplied too large of a network. v4 max >= {v4_min_prefix} v6 >= {v6_min_prefix}"
     default_code = "prefix_too_large"
