854e61592337ac6a4e0d9c242ab3e8a6859054aa
Sam Oehlert <sam.oehlert@gmail.com>
2022-02-28 15:28:49 -0600

    style(flake8): fix flake8 bug about fstrings

--- scram/route_manager/api/exceptions.py
+++ scram/route_manager/api/exceptions.py
@@ -3,8 +3,8 @@ from rest_framework.exceptions import APIException
 
 
 class PrefixTooLarge(APIException):
-    v4_min_prefix = getattr(settings, f"V4_MINPREFIX", 0)
-    v6_min_prefix = getattr(settings, f"V6_MINPREFIX", 0)
+    v4_min_prefix = getattr(settings, "V4_MINPREFIX", 0)
+    v6_min_prefix = getattr(settings, "V6_MINPREFIX", 0)
     status_code = 400
     default_detail = f"You've supplied too large of a network. v4 max >= {v4_min_prefix} v6 >= {v6_min_prefix}"
     default_code = "prefix_too_large"
