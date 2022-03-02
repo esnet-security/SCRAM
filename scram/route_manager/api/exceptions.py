9e43e44ccef0002c57efb0f730b70bb14b04db2d
Sam Oehlert <sam.oehlert@gmail.com>
2022-03-02 11:46:43 -0600

    style(typo): fix typoes in error message from DRF

--- scram/route_manager/api/exceptions.py
+++ scram/route_manager/api/exceptions.py
@@ -6,5 +6,5 @@ class PrefixTooLarge(APIException):
     v4_min_prefix = getattr(settings, "V4_MINPREFIX", 0)
     v6_min_prefix = getattr(settings, "V6_MINPREFIX", 0)
     status_code = 400
-    default_detail = f"You've supplied too large of a network based. settings.V4_MINPREFIX = {v4_min_prefix} settings.V6_MINPREFIX= {v6_min_prefix}"  # noqa: 501
+    default_detail = f"You've supplied too large of a network. settings.V4_MINPREFIX = {v4_min_prefix} settings.V6_MINPREFIX = {v6_min_prefix}"  # noqa: 501
     default_code = "prefix_too_large"
