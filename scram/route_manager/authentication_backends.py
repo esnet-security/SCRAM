bd8cf67bf8e113d784da7c95c1082b2ea03e63f4
Vlad Grigorescu <vlad@es.net>
2022-03-02 20:23:58 +0000

    Only update the groups if we have groups in the claim.

--- scram/route_manager/authentication_backends.py
+++ scram/route_manager/authentication_backends.py
@@ -44,7 +44,8 @@ class ESnetAuthBackend(OIDCAuthenticationBackend):
     def update_user(self, user, claims):
         user.name = claims.get("given_name", "") + " " + claims.get("family_name", "")
         user.username = claims.get("preferred_username", "")
-        self.update_groups(user, claims)
+        if claims.get("groups", False]):
+            self.update_groups(user, claims)
 
         user.save()
 
