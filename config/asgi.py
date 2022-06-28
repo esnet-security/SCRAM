9e10ec2c14472d149bb9ec0482c17ea860d087fb
Vlad Grigorescu <vlad@es.net>
2022-06-28 10:24:28 -0500

    Enable channels

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,7 @@ import os
 import sys
 from pathlib import Path
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 # This allows easy placement of apps within the interior
@@ -27,14 +28,19 @@ django_application = get_asgi_application()
 # from helloworld.asgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
 
-# Import websocket application here, so apps from django_application are loaded first
-from config.websocket import websocket_application  # noqa isort:skip
+application = ProtocolTypeRouter({
+    "http": django_application,
+    # Just HTTP for now. (We can add other protocols later.)
+})
+
+# # Import websocket application here, so apps from django_application are loaded first
+# from config.websocket import websocket_application  # noqa isort:skip
 
 
-async def application(scope, receive, send):
-    if scope["type"] == "http":
-        await django_application(scope, receive, send)
-    elif scope["type"] == "websocket":
-        await websocket_application(scope, receive, send)
-    else:
-        raise NotImplementedError(f"Unknown scope type {scope['type']}")
+# async def application(scope, receive, send):
+#     if scope["type"] == "http":
+#         await django_application(scope, receive, send)
+#     elif scope["type"] == "websocket":
+#         await websocket_application(scope, receive, send)
+#     else:
+#         raise NotImplementedError(f"Unknown scope type {scope['type']}")
