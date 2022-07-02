a9244ca3865ff253ec17eedef5b33f10ae316aca
Sam Oehlert <sam.oehlert@gmail.com>
2022-07-01 22:00:29 -0500

    feat(ignoreentry): add model to admin page

--- scram/route_manager/admin.py
+++ scram/route_manager/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ActionType, Entry, History, Route
+from .models import ActionType, Entry, History, IgnoreEntry, Route
 
 
 @admin.register(ActionType)
@@ -11,4 +11,5 @@ class ActionTypeAdmin(admin.ModelAdmin):
 
 admin.site.register(Entry)
 admin.site.register(History)
+admin.site.register(IgnoreEntry)
 admin.site.register(Route)
