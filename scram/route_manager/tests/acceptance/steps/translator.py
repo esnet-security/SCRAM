ea88ba58728c7843aeb389a8bfb00f16c6dfbc75
Sam Oehlert <sam.oehlert@gmail.com>
2022-07-06 12:29:51 -0500

    refactor(naming): use translator as the name for the actual communication as well

--- scram/route_manager/tests/acceptance/steps/translator.py
+++ scram/route_manager/tests/acceptance/steps/translator.py
@@ -5,13 +5,13 @@ from channels.testing import WebsocketCommunicator
 from config.asgi import ws_application
 
 
-async def query_xlator(route, actiontype, is_announced=True):
+async def query_translator(route, actiontype, is_announced=True):
     communicator = WebsocketCommunicator(ws_application, f"/ws/route_manager/webui_{actiontype}/")
     connected, subprotocol = await communicator.connect()
     assert connected
 
     await communicator.send_json_to({"type": "check_block_req", "message": {"route": route}})
-    response = await communicator.receive_json_from()
+    response = await communicator.receive_json_from(timeout=3)
     assert response['type'] == 'check_block_resp'
     assert response['message']['is_blocked'] == is_announced
 
@@ -21,10 +21,10 @@ async def query_xlator(route, actiontype, is_announced=True):
 @then("{route} is announced by {actiontype} translators")
 @async_run_until_complete
 async def step_impl(context, route, actiontype):
-    await query_xlator(route, actiontype)
+    await query_translator(route, actiontype)
 
 
 @then("{route} is not announced by {actiontype} translators")
 @async_run_until_complete
 async def step_impl(context, route, actiontype):
-    await query_xlator(route, actiontype, is_announced=False)
+    await query_translator(route, actiontype, is_announced=False)
