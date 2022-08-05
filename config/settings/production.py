9c7af480867d77b57b9e8a0278a253680f02e6be
Sam Oehlert <sam.oehlert@gmail.com>
2022-08-05 13:24:35 -0500

    fix(flake8): ignore flake8 warning like the other db related lines

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,7 +16,7 @@ DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 if env.POSTGRES_SSL:
-    DATABASES["default"]["OPTIONS"] = {'sslmode': 'require'}
+    DATABASES["default"]["OPTIONS"] = {'sslmode': 'require'}  # noqa F405
 
 # CACHES
 # ------------------------------------------------------------------------------
