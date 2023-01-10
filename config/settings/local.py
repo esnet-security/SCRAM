6263ec8d6c7b6a51e36a0e3268f7695a80a15a92
Sam Oehlert <soehlert@es.net>
2023-01-10 17:48:38 -0600

    fix(django-process_expired): add django container by name to allowed_hosts in dev
    It was still working but showing lots of errors when we ran process_expired without that

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="BmZnn8FeNFdaeCod8ky6eBNpTiwO45NzlFyA6kk1xo0g4Mc263gAyscHFCMCeJAi",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]
 
 # CACHES
 # ------------------------------------------------------------------------------
