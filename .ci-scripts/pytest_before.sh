fed852f3a622a89ceac278cd4dda83fd26948a83
Vlad Grigorescu <vlad@es.net>
2024-01-05 17:01:09 -0600

    GitHub actions incremental

--- .ci-scripts/pytest_before.sh
+++ .ci-scripts/pytest_before.sh
@@ -2,20 +2,19 @@
 
 set -e
 
-env
-
 if [ -z "${CI_REGISTRY}" ]; then
     # Running in GitHub
     export COMPOSE_PROJECT_NAME=$GITHUB_RUN_ID
+    make build
 else
     # Running in Gitlab
     apk add make
     docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
     export COMPOSE_PROJECT_NAME=$CI_PIPELINE_ID
+    .ci-scripts/pull_images.sh
+    make build
+    .ci-scripts/push_images.sh
 fi
 
-.ci-scripts/pull_images.sh
-make build
-.ci-scripts/push_images.sh
 make migrate
 make run
