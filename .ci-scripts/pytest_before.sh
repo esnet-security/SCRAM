ea882518908f7d343311fc083b4327e3e43f7993
Vlad Grigorescu <vlad@es.net>
2024-01-05 16:48:55 -0600

    GitHub isn't using alpine

--- .ci-scripts/pytest_before.sh
+++ .ci-scripts/pytest_before.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-apk add make
-docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-export COMPOSE_PROJECT_NAME=$CI_PIPELINE_ID
+if [ -z "${CI_REGISTRY}" ]; then
+    # GitHub uses Ubuntu
+    apk add make
+    docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+    export COMPOSE_PROJECT_NAME=$CI_PIPELINE_ID
+else
+    export COMPOSE_PROJECT_NAME=$GITHUB_REPOSITORY
+fi
+
 .ci-scripts/pull_images.sh
 make build
 .ci-scripts/push_images.sh
