f22f2e813b1b0fe5d98dd77cd22aa668220c0100
Sam Oehlert <soehlert@es.net>
2024-11-14 15:32:12 -0600

    refactor(auth): move auth_method to production which is the only file we use it in
    also appeases flake8

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from .base import *  # noqa
@@ -141,44 +142,64 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-# Extend middleware to add OIDC middleware
-MIDDLEWARE += ["mozilla_django_oidc.middleware.SessionRefresh"]  # noqa F405
+# Are you using local passwords or oidc?
+AUTH_METHOD = os.environ.get("SCRAM_AUTH_METHOD", "local").lower()
 
-# Extend middleware to add OIDC auth backend
-AUTHENTICATION_BACKENDS += ["scram.route_manager.authentication_backends.ESnetAuthBackend"]  # noqa F405
+logging.info(f"Using AUTH METHOD = {AUTH_METHOD}")
+if AUTH_METHOD == "oidc":
+    # Extend middleware to add OIDC middleware
+    MIDDLEWARE += ["mozilla_django_oidc.middleware.SessionRefresh"]  # noqa F405
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-url
-LOGIN_URL = "oidc_authentication_init"
+    # Extend middleware to add OIDC auth backend
+    AUTHENTICATION_BACKENDS += ["scram.route_manager.authentication_backends.ESnetAuthBackend"]  # noqa F405
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "/"
+    # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
+    LOGIN_URL = "oidc_authentication_init"
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#logout-url
-LOGOUT_URL = "oidc_logout"
+    # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
+    LOGIN_REDIRECT_URL = "/"
 
-# Need to point somewhere otherwise /oidc/logout/ redirects to /oidc/logout/None which 404s
-# https://github.com/mozilla/mozilla-django-oidc/issues/118
-# Using `/` because named urls don't work for this package
-# https://github.com/mozilla/mozilla-django-oidc/issues/434
-LOGOUT_REDIRECT_URL = "/"
+    # https://docs.djangoproject.com/en/dev/ref/settings/#logout-url
+    LOGOUT_URL = "oidc_logout"
 
-OIDC_OP_JWKS_ENDPOINT = os.environ.get(
-    "OIDC_OP_JWKS_ENDPOINT",
-    "https://example.com/auth/realms/example/protocol/openid-connect/certs",
-)
-OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get(
-    "OIDC_OP_AUTHORIZATION_ENDPOINT",
-    "https://example.com/auth/realms/example/protocol/openid-connect/auth",
-)
-OIDC_OP_TOKEN_ENDPOINT = os.environ.get(
-    "OIDC_OP_TOKEN_ENDPOINT",
-    "https://example.com/auth/realms/example/protocol/openid-connect/token",
-)
-OIDC_OP_USER_ENDPOINT = os.environ.get(
-    "OIDC_OP_USER_ENDPOINT",
-    "https://example.com/auth/realms/example/protocol/openid-connect/userinfo",
-)
-OIDC_RP_SIGN_ALGO = "RS256"
+    # Need to point somewhere otherwise /oidc/logout/ redirects to /oidc/logout/None which 404s
+    # https://github.com/mozilla/mozilla-django-oidc/issues/118
+    # Using `/` because named urls don't work for this package
+    # https://github.com/mozilla/mozilla-django-oidc/issues/434
+    LOGOUT_REDIRECT_URL = "/"
 
-OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID")
-OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET")
+    OIDC_OP_JWKS_ENDPOINT = os.environ.get(
+        "OIDC_OP_JWKS_ENDPOINT",
+        "https://example.com/auth/realms/example/protocol/openid-connect/certs",
+    )
+    OIDC_OP_AUTHORIZATION_ENDPOINT = os.environ.get(
+        "OIDC_OP_AUTHORIZATION_ENDPOINT",
+        "https://example.com/auth/realms/example/protocol/openid-connect/auth",
+    )
+    OIDC_OP_TOKEN_ENDPOINT = os.environ.get(
+        "OIDC_OP_TOKEN_ENDPOINT",
+        "https://example.com/auth/realms/example/protocol/openid-connect/token",
+    )
+    OIDC_OP_USER_ENDPOINT = os.environ.get(
+        "OIDC_OP_USER_ENDPOINT",
+        "https://example.com/auth/realms/example/protocol/openid-connect/userinfo",
+    )
+    OIDC_RP_SIGN_ALGO = "RS256"
+
+    OIDC_RP_CLIENT_ID = os.environ.get("OIDC_RP_CLIENT_ID")
+    OIDC_RP_CLIENT_SECRET = os.environ.get("OIDC_RP_CLIENT_SECRET")
+
+elif AUTH_METHOD == "local":
+    # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
+    LOGIN_URL = "/login"
+
+    # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
+    LOGIN_REDIRECT_URL = "route_manager:home"
+
+    # https://docs.djangoproject.com/en/dev/ref/settings/#logout-url
+    LOGOUT_URL = "/logout"
+
+    # https://docs.djangoproject.com/en/dev/ref/settings/#logout-redirect-url
+    LOGOUT_REDIRECT_URL = "/"
+else:
+    raise Exception(f"Invalid authentication method: {AUTH_METHOD}. Please choose 'local' or 'oidc'")
