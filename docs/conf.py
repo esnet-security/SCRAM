1b8a44bbaebe85756f64caf2dbd13e9ce77e75a7
Vlad Grigorescu <vlad@es.net>
2024-11-18 09:10:18 -0600

    Don't generate docs for the Django migrations.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**/*.migrations.rst"]
 
 # -- Options for HTML output -------------------------------------------------
 
