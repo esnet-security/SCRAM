6a4c8e2ca9225424262a193348fbe3bd0ecd4eda
Vlad Grigorescu <vlad@es.net>
2024-11-23 19:46:39 -0600

    Add a few meaningful tests to the homepage

--- scram/route_manager/tests/test_views.py
+++ scram/route_manager/tests/test_views.py
@@ -1,7 +1,7 @@
 """Define simple tests for the template-based Views."""
 
 from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
 
 from scram.route_manager.views import home_page
 
@@ -13,3 +13,19 @@ class HomePageTest(TestCase):
         """Ensure we can find the home page."""
         found = resolve("/")
         self.assertEqual(found.func, home_page)
+
+
+class HomePageFirstVisitTest(TestCase):
+    """Test how the home page renders the first time we view it."""
+
+    def setUp(self):
+        """Get the home page."""
+        self.response = self.client.get(reverse("route_manager:home"))
+
+    def test_first_homepage_view_has_userinfo(self):
+        """The first time we view the home page, a user was created for us."""
+        self.assertContains(self.response, b"An admin user was created for you.")
+
+    def test_first_homepage_view_is_logged_in(self):
+        """The first time we view the home page, we're logged in."""
+        self.assertContains(self.response, b'type="submit">Logout')
