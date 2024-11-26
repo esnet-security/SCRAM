6ad3b355b5c29e79822eeb04425229b3de232f6b
Vlad Grigorescu <vlad@es.net>
2024-11-26 09:54:30 -0600

    Change the validator to a warning.

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,11 @@
 """Define the configuration scheme using environ-config."""
 
+import logging
+
 import environ
 from attrs.validators import in_
+
+logger = logging.getLogger(__name__)
 
 
 @environ.config(prefix="SCRAM")
@@ -10,9 +14,12 @@ class AppConfig:
 
     debugger = environ.var(
         default="",
-        help='Debug SCRAM with a debugger. Set to either "pycharm-pydevd" or "debugpy".',
-        validator=in_(["pycharm-pydevd", "debugpy", ""]),
-    )
+        help='Will launch the appropriate debugger if set to either "pycharm-pydevd" or "debugpy".')
+    
+    @debugger.validator
+    def _warn_on_unknown_debugger(self, var, debugger):
+        if debugger and debugger not in {"pycharm-pydevd", "debugpy"}:
+            logger.warning("Unknown debugger value: %s", debugger)
 
 
 cfg = environ.to_config(AppConfig)
--- docs/reference/index.md
+++ docs/reference/index.md
@@ -2,8 +2,6 @@
 
 The SCRAM ecosystem consists of two parts:
 
-A Django app, [route_manager](/reference/django.md)
+A Django app, [route_manager](django.md)
 
-A translator service, [translator](/reference/translator.md)
-
-Environment variable configuration [envvars](/reference/envvars.md)
+A translator service, [translator](translator.md)
