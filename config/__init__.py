2353d83d4c6189fa2e6f673a9c251401e7275c03
Vlad Grigorescu <vlad@es.net>
2024-11-26 09:55:17 -0600

    Ruff fixes

--- config/__init__.py
+++ config/__init__.py
@@ -3,7 +3,6 @@
 import logging
 
 import environ
-from attrs.validators import in_
 
 logger = logging.getLogger(__name__)
 
@@ -13,9 +12,9 @@ class AppConfig:
     """Top level configuration (i.e. SCRAM_)."""
 
     debugger = environ.var(
-        default="",
-        help='Will launch the appropriate debugger if set to either "pycharm-pydevd" or "debugpy".')
-    
+        default="", help='Will launch the appropriate debugger if set to either "pycharm-pydevd" or "debugpy".'
+    )
+
     @debugger.validator
     def _warn_on_unknown_debugger(self, var, debugger):
         if debugger and debugger not in {"pycharm-pydevd", "debugpy"}:
