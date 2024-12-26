d71aa6f597fca2cf5333069edd44eacf63ef06c6
Sam Oehlert <soehlert@es.net>
2024-12-25 22:08:15 -0600

    feat(entry admin): Improve the UX on the entry admin panel (#108)
    Closes #99 
    Closes #98 
    
    I had to branch off of this to make some other changes as well
    unfortunately just to get a working copy of the stack. Our expiration
    was not TZ aware which broke any add (from WUI or admin). Also had to
    fix the translatorConsumer because websockets were broken and spewing
    errors constantly.

--- scram/route_manager/admin.py
+++ scram/route_manager/admin.py
@@ -6,6 +6,31 @@ from simple_history.admin import SimpleHistoryAdmin
 from .models import ActionType, Client, Entry, IgnoreEntry, Route, WebSocketMessage, WebSocketSequenceElement
 
 
+class WhoFilter(admin.SimpleListFilter):
+    """Only display users who have added entries in the list_filter."""
+
+    title = "By Username"
+    parameter_name = "who"
+
+    # ruff: noqa: PLR6301
+    def lookups(self, request, model_admin):
+        """Return list of users who have added entries."""
+        users_with_entries = Entry.objects.values("who").distinct()
+
+        # If no users have entries, return an empty list so they don't show in filter
+        if not users_with_entries:
+            return []
+
+        # Return a list of users who have made entries
+        return [(user["who"], user["who"]) for user in users_with_entries]
+
+    def queryset(self, request, queryset):
+        """Queryset for users."""
+        if self.value():
+            return queryset.filter(who=self.value())
+        return queryset
+
+
 @admin.register(ActionType)
 class ActionTypeAdmin(SimpleHistoryAdmin):
     """Configure the ActionType and how it shows up in the Admin site."""
@@ -14,7 +39,17 @@ class ActionTypeAdmin(SimpleHistoryAdmin):
     list_display = ("name", "available")
 
 
-admin.site.register(Entry, SimpleHistoryAdmin)
+@admin.register(Entry)
+class EntryAdmin(SimpleHistoryAdmin):
+    """Configure how Entries show up in the Admin site."""
+
+    list_filter = [
+        "is_active",
+        WhoFilter,
+    ]
+    search_fields = ["route", "comment"]
+
+
 admin.site.register(IgnoreEntry, SimpleHistoryAdmin)
 admin.site.register(Route)
 admin.site.register(Client)
