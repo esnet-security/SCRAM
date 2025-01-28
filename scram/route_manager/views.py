619fdf88fd11177caaed1f33022d153a8d2abaf9
Sam Oehlert <soehlert@es.net>
2025-01-28 12:04:36 -0600

    refactor(extraneous-code): remove extraneous queryset line and also remove total since we dont display that anywhere anymore

--- scram/route_manager/views.py
+++ scram/route_manager/views.py
@@ -35,11 +35,9 @@ def home_page(request, prefilter=None):
         readwrite = False
     context = {"entries": {}, "readwrite": readwrite}
     for at in ActionType.objects.all():
-        queryset = prefilter.filter(actiontype=at).order_by("-pk")
-        queryset_active = queryset.filter(is_active=True).order_by("-pk")
+        queryset_active = prefilter.filter(actiontype=at, is_active=True)
         context["entries"][at] = {
             "objs": queryset_active[:num_entries],
-            "total": queryset.count(),
             "active": queryset_active.count(),
         }
 
