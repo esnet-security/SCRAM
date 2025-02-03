913e43199236690c1ed51ea1f7176b5031117fb5
Sam Oehlert <soehlert@es.net>
2025-02-03 15:19:30 -0600

    feat(active_blocks): Add a quick way to tell how many active blocks there are (#127)
    Closes #30
    
    This adds a pill badge to the navbar with a quick view at active blocks.
    Also, it filters the homepage to only show active blocks and update the
    pill badge above the actiontype list on the homepage to only show the
    count of active blocks. The navbar is specific to blocks, but that seems
    fair since 1) that's the main use case right now 2) that's the only
    default actiontype anyways. We can always make it nicer/configurable in
    the future if needed.

--- scram/route_manager/tests/test_performance.py
+++ scram/route_manager/tests/test_performance.py
@@ -26,30 +26,31 @@ class TestViewNumQueries(TestCase):
         self.atype, _ = ActionType.objects.get_or_create(name="block")
         routes = [Route(route=self.fake.unique.ipv4_public()) for x in range(self.NUM_ENTRIES)]
         created_routes = Route.objects.bulk_create(routes)
-        entries = [Entry(route=route, actiontype=self.atype) for route in created_routes]
+        entries = [Entry(route=route, actiontype=self.atype, is_active=True) for route in created_routes]
         Entry.objects.bulk_create(entries)
 
     def test_home_page(self):
-        """Home page requires 8 queries.
+        """Home page requires 11 queries.
 
         1. create transaction
         2. lookup session
         3. lookup user
         4. filter available actiontypes
-        5. count entries with actiontype=1
+        5. count entries with actiontype=1 and is_active
         6. count by user
-        7. first page for actiontype=1
-        8. close transaction
-
+        7. context processor active_count active blocks
+        8. context processor active_count all blocks
+        9. first page for actiontype=1
+        10. close transaction
         """
-        with self.assertNumQueries(8):
+        with self.assertNumQueries(10):
             start = time.time()
             self.client.get(reverse("route_manager:home"))
             time_taken = time.time() - start
             self.assertLess(time_taken, 1, "Home page took longer than 1 second")
 
     def test_entry_view(self):
-        """Viewing an entry requires 6 queries.
+        """Viewing an entry requires 8 queries.
 
         1. create transaction savepoint
         2. lookup session
@@ -57,9 +58,10 @@ class TestViewNumQueries(TestCase):
         4. get entry
         5. rollback to savepoint
         6. release transaction savepoint
-
+        7. context processor active_count active blocks
+        8. context processor active_count all blocks
         """
-        with self.assertNumQueries(6):
+        with self.assertNumQueries(8):
             start = time.time()
             self.client.get(reverse("route_manager:detail", kwargs={"pk": 9999}))
             time_taken = time.time() - start
