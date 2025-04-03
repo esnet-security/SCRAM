591ebd9f1e07853ec686ad4e8ef77c9c1c145d08
Sam Oehlert <soehlert@es.net>
2025-04-03 14:30:55 -0500

    refactor(ruff): ignore ruff warning since its not correctly interpreting our code

--- config/settings/local.py
+++ config/settings/local.py
@@ -55,6 +55,7 @@ if env("USE_DOCKER", default="no") == "yes":
     import socket
 
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    # ruff: noqa: RUF005
     INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 
 # django-extensions
