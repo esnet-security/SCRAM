6d829bd995861f6b8ce4d87ef9de2235bd4e4bb5
chriscummings <chriscummings@es.net>
2025-07-07 16:01:42 -0500

    fix(login/logout url): use redirect instead of reverse

--- scram/route_manager/context_processors.py
+++ scram/route_manager/context_processors.py
@@ -1,7 +1,7 @@
 """Define custom functions that take a request and add to the context before template rendering."""
 
 from django.conf import settings
-from django.urls import reverse
+from django.urls import redirect
 
 from scram.route_manager.models import Entry
 
@@ -12,8 +12,8 @@ def login_logout(request):
     Returns:
        dict: login and logout URLs
     """
-    login_url = reverse(settings.LOGIN_URL)
-    logout_url = reverse(settings.LOGOUT_URL)
+    login_url = redirect(settings.LOGIN_URL)
+    logout_url = redirect(settings.LOGOUT_URL)
     return {"login": login_url, "logout": logout_url}
 
 
