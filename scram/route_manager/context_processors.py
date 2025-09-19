1b5be9c387cc508f26a1cd8229dcfa701630916a
Sam Oehlert <sam.oehlert@gmail.com>
2025-09-19 12:43:00 -0500

    fix(login/out/redirect): edit the log{in|out}_urls and log{in|out}_redirect_urls to fix the local auth and hopefully the oidc
    added some debugging that will go away once its working

--- scram/route_manager/context_processors.py
+++ scram/route_manager/context_processors.py
@@ -2,6 +2,7 @@
 
 from django.conf import settings
 from django.urls import reverse
+from django.shortcuts import redirect
 
 from scram.route_manager.models import Entry
 
@@ -12,8 +13,19 @@ def login_logout(request):
     Returns:
        dict: login and logout URLs
     """
-    login_url = reverse(settings.LOGIN_URL)
-    logout_url = reverse(settings.LOGOUT_URL)
+    if ":" in settings.LOGIN_URL:
+        print(f"WTFFFFFFFFFFFFFFFFFFFFFFF: {settings.LOGIN_URL}")
+        login_url = reverse(settings.LOGIN_URL)
+        logout_url = reverse(settings.LOGOUT_URL)
+    else:
+        login_url = redirect(settings.LOGIN_URL)
+        logout_url = redirect(settings.LOGOUT_URL)
+
+    print(f"REDIREC OUT: {settings.LOGOUT_REDIRECT_URL}")
+    print(f"REDIRECT IN: {settings.LOGIN_REDIRECT_URL}")
+    print(f"LOGOUT_URL: {logout_url}")
+    print(f"LOGIN_URL: {login_url}")
+
     return {"login": login_url, "logout": logout_url}
 
 
