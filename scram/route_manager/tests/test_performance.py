94438f409ef6beed3729720b02c7aa47d65dfbb9
chriscummings <chriscummings@es.net>
2025-10-16 10:28:05 -0500

    ci: whoops remove the correct test.

--- scram/route_manager/tests/test_performance.py
+++ scram/route_manager/tests/test_performance.py
@@ -53,24 +53,23 @@ class TestViewNumQueries(TestCase):
             time_taken = time.time() - start
             self.assertLess(time_taken, 1, "Home page took longer than 1 second")
 
-    def test_admin_entry_page(self):
-        """Admin entry list page requires 8 queries.
+    def test_entry_view(self):
+        """Viewing an entry requires 8 queries.
 
-        1. create transaction
+        1. create transaction savepoint
         2. lookup session
         3. lookup user
-        4. lookup distinct users for our WhoFilter
-        4. count entries
-        5. count entries
-        6. get first 100 entries
-        7. query entries (a single query, with select_related)
-        8. release transaction
+        4. get entry
+        5. rollback to savepoint
+        6. release transaction savepoint
+        7. context processor active_count active blocks
+        8. context processor active_count all blocks
         """
         with self.assertNumQueries(8):
             start = time.time()
-            self.client.get(reverse("admin:route_manager_entry_changelist"))
+            self.client.get(reverse("route_manager:detail", kwargs={"pk": 9999}))
             time_taken = time.time() - start
-            self.assertLess(time_taken, 1, "Admin entry list page took longer than 1 seconds")
+            self.assertLess(time_taken, 1, "Entry detail page took longer than 1 second")
 
     def test_process_updates(self):
         """Process expired requires 6 queries.
