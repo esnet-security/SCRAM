b2030215a12a60a063a0a7f6e7c9f8e60b40dcbc
Sam Oehlert <sam.oehlert@gmail.com>
2025-12-19 11:20:08 -0600

    fix(UUID-registration): add the UUID field to the serializer
    this lets people register if they add one to the registration POST. it was just set to autocreate one only. this is more flexible.

--- scram/route_manager/api/serializers.py
+++ scram/route_manager/api/serializers.py
@@ -44,6 +44,7 @@ class RouteSerializer(serializers.ModelSerializer):
 
 class ClientSerializer(serializers.ModelSerializer):
     """Map the serializer to the model via Meta."""
+    uuid = serializers.UUIDField()
 
     class Meta:
         """Maps to the Client model, and specifies the fields exposed by the API."""
