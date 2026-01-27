26db9800e66447d7c6fe72f8d5d6a72a1bc22ef1
crankynetman <chriscummings@es.net>
2026-01-27 08:45:34 -0600

    fix(tasks): docstring typos

--- scheduler/src/scheduler/tasks.py
+++ scheduler/src/scheduler/tasks.py
@@ -11,15 +11,15 @@ from .settings import settings
 
 @scram_api_scheduler.task
 def perform_process_updates() -> dict[Any, Any]:
-    """Trigger the SCRAM /process_endpoints API endpoint to process pending updates.
+    """Trigger the SCRAM /process_updates API endpoint to process pending updates.
 
-    Makes a GET request to the /process_updates endpoint with a timeout 5 seconds
-    shorter than the process_updates_interval setting to prevent stacking up calls.
+    Makes a GET request to the /process_updates endpoint with a timeout shorter than
+    the process_updates_interval setting to prevent stacking up calls.
 
     Returns:
-        dict: Response containing status and timestamp on success.
+        dict: Response containing api response, success status, and timestamp.
     """
-    timeout = settings.process_updates_interval - 5
+    timeout = settings.process_updates_interval - settings.process_updates_timeout_interval
     with httpx.Client() as client:
         response = client.get(f"{settings.scram_api_url}process_updates/", timeout=timeout)
         response.raise_for_status()
