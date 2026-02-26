fab45c5b98c179104ff784a94251824dbebeb683
crankynetman <chriscummings@es.net>
2026-02-26 15:10:38 -0600

    forgot a change

--- django/src/config/wsgi.py
+++ django/src/config/wsgi.py
@@ -17,7 +17,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# config/ and scram/ are siblings under src/, no sys.path manipulation needed.
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
