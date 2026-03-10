7eeb573ee7e9a366ec86f70697323eba818dc09e
Chris Cummings <chriscummings@es.net>
2026-03-10 15:28:52 -0500

    refactor(settings): move translator to pydantic settings (#221)
    closes #48 and gives us nice clean pydantic settings for all the
    validation of our translator settings, the same as `scheduler` does.

---
+++ translator/src/translator/settings.py
@@ -0,0 +1,70 @@
+"""Application settings loaded from environment variables."""
+
+from enum import StrEnum
+from typing import Annotated
+
+from pydantic import AnyWebsocketUrl, computed_field, field_validator
+from pydantic.fields import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+TCPUDPPort = Annotated[int, Field(ge=1, le=65535)]
+
+
+class DebuggerTypes(StrEnum):
+    """Supported debuggers."""
+
+    PYCHARM_PYDEVD = "pycharm-pydevd"
+    DEBUGPY = "debugpy"
+
+
+class Settings(BaseSettings):
+    """SCRAM Translator settings."""
+
+    # make sure that empty strings are not considered as having provided a value.
+    model_config = SettingsConfigDict(env_ignore_empty=True)
+
+    log_level: str = "INFO"
+    debug: DebuggerTypes | None = None
+
+    # GoBGP Connection Specifics
+    gobgp_host: str = "gobgp"
+    gobgp_port: TCPUDPPort = 50051
+
+    @computed_field
+    @property
+    def gobgp_url(self) -> str:
+        """Return the composed GoBGP gRPC URL."""
+        return f"{self.gobgp_host}:{self.gobgp_port}"
+
+    # SCRAM Connection Specifics
+    scram_hostname: str = "scram_hostname_not_set"
+    scram_events_url: str = "ws://django:8000/ws/route_manager/translator_block/"
+
+    # GoBGP ASpath defaults (fallback values when event_data doesn't provide them)
+    default_asn: int = 65400
+    default_community: int = 666
+    default_v4_nexthop: str = "192.0.2.199"
+    default_v6_nexthop: str = "100::1"
+
+    # Custom Validator(s)
+    @field_validator("scram_events_url")
+    @classmethod
+    def _validate_scram_events_url(cls, v: str) -> str:
+        """Validate that the url is a valid WebSocket URL *and* has a trailing slash.
+
+        Returns:
+            str: The validated WebSocket URL with a trailing slash.
+
+        Raises:
+            ValueError: If the URL is not a valid WebSocket URL or does not have a trailing slash.
+        """
+        # check to make sure that this is actually a valid websocket URL
+        parsed_url = AnyWebsocketUrl(v)
+        # then make sure that the trailing slash is actually there.
+        if not (parsed_url.path and parsed_url.path.endswith("/")):
+            msg = f"scram_events_url: {v!r} must have a trailing slash"
+            raise ValueError(msg)
+        return v
+
+
+settings = Settings()  # type: ignore[call-arg]
--- translator/src/translator/translator.py
+++ translator/src/translator/translator.py
@@ -6,20 +6,14 @@ import asyncio
 import ipaddress
 import json
 import logging
-import os
-from os import getenv
 
 import websockets
 from grpc import RpcError
 
 from .gobgp import GoBGP
+from .settings import DebuggerTypes, settings
 
-LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
-GOBGP_HOST = getenv("GOBGP_HOST", "gobgp")
-GOBGP_PORT = getenv("GOBGP_PORT", "50051")
-GOBGP_URL = GOBGP_HOST + ":" + GOBGP_PORT
-
-logging.basicConfig(level=LOG_LEVEL)
+logging.basicConfig(level=settings.log_level)
 logger = logging.getLogger(__name__)
 
 KNOWN_MESSAGES = {
@@ -30,33 +24,27 @@ KNOWN_MESSAGES = {
 }
 
 # Here we setup a debugger if this is desired. This obviously should not be run in production.
-debug_mode = os.environ.get("DEBUG")
-if debug_mode:
-    logger.info("Translator is set to use a debugger. Provided debug mode: %s", debug_mode)
+if settings.debug:
+    logger.info("Translator is set to use a debugger. Provided debug mode: %s", settings.debug)
     # We have to setup the debugger appropriately for various IDEs. It'd be nice if they all used the same thing but
     # sadly, we live in a fallen world.
-    if debug_mode == "pycharm-pydevd":
-        logger.info("Entering debug mode for pycharm, make sure the debug server is running in PyCharm!")
+    match settings.debug:
+        case DebuggerTypes.PYCHARM_PYDEVD:
+            logger.info("Entering debug mode for pycharm, make sure the debug server is running in PyCharm!")
 
-        import pydevd_pycharm
+            import pydevd_pycharm
 
-        pydevd_pycharm.settrace("host.docker.internal", port=56782, stdoutToServer=True, stderrToServer=True)
+            pydevd_pycharm.settrace("host.docker.internal", port=56782, stdoutToServer=True, stderrToServer=True)
 
-        logger.info("Debugger started.")
-    elif debug_mode == "debugpy":
-        logger.info("Entering debug mode for debugpy (VSCode)")
+            logger.info("Debugger started.")
+        case DebuggerTypes.DEBUGPY:
+            logger.info("Entering debug mode for debugpy (VSCode)")
 
-        import debugpy
+            import debugpy
 
-        debugpy.listen(("0.0.0.0", 56781))  # noqa S104 (doesn't like binding to all interfaces)
+            debugpy.listen(("0.0.0.0", 56781))  # noqa S104 (doesn't like binding to all interfaces)
 
-        logger.info("Debugger listening on port 56781.")
-    else:
-        logger.warning("Invalid debug mode given: %s. Debugger not started", debug_mode)
-
-# Must match the URL in asgi.py, and needs a trailing slash
-hostname = os.environ.get("SCRAM_HOSTNAME", "scram_hostname_not_set")
-url = os.environ.get("SCRAM_EVENTS_URL", "ws://django:8000/ws/route_manager/translator_block/")
+            logger.info("Debugger listening on port 56781.")
 
 
 async def process(message, websocket, g):
@@ -83,15 +71,15 @@ async def process(message, websocket, g):
         elif event_type == "translator_check":
             json_message["type"] = "translator_check_resp"
             json_message["message"]["is_blocked"] = g.is_blocked(ip)
-            json_message["message"]["translator_name"] = hostname
+            json_message["message"]["translator_name"] = settings.scram_hostname
             await websocket.send(json.dumps(json_message))
 
 
 async def websocket_loop():
     """Connect to the websocket and start listening for messages for Gobgp."""
-    logger.info("connecting to gobgp at %s", GOBGP_URL)
-    g = GoBGP(GOBGP_URL)
-    async for websocket in websockets.connect(url):
+    logger.info("connecting to gobgp at %s", settings.gobgp_url)
+    g = GoBGP(settings.gobgp_url)
+    async for websocket in websockets.connect(settings.scram_events_url):
         try:
             async for message in websocket:
                 await process(message, websocket, g)
